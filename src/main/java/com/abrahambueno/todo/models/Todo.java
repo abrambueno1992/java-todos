c3062f3bc0716892760e2c8e1ef203c193d5ae92
Abraham Bueno <abueno@localhost.localdomain>
2019-02-22 11:21:54 -0600

    Initial setup of models

--- src/main/java/com/abrahambueno/todo/models/Todo.java
+++ src/main/java/com/abrahambueno/todo/models/Todo.java
@@ -1,4 +1,19 @@
 package com.abrahambueno.todo.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todo")
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    private String datestarted;
+    private boolean completed;
+
+    // user id
 }
--- src/main/java/com/abrahambueno/todo/models/User.java
+++ src/main/java/com/abrahambueno/todo/models/User.java
@@ -1,4 +1,14 @@
 package com.abrahambueno.todo.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userid;
+
+    @Column(nullable = false)
+    private String username;
 }
